53a2e7dba5b87f54f82267cb93ab65d57fb50625
rajchaudar <rajchaudar60@gmail.com>
2025-04-23 18:52:50 +0530

    refactor: update MongoDB connection string to use environment variable

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const green = '\x1b[32m';
 async function connectDB() {
     try {
-        await mongoose.connect('mongodb+srv://rajchaudar60:raj1415@cluster0.ozz8e.mongodb.net/EmployeeDB');
+        await mongoose.connect(process.env.MONGO_URI);
         console.log(`${green}MongoDB Connection Succeeded.`);
     } catch (error) {
         console.error('Error in DB connection: ' + error);
